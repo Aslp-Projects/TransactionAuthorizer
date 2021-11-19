37da2f7ae362936e08a960f2d743913ad401f017
AsLopez <and3.s@hotmail.com>
2021-11-19 18:01:18 -0500

    move commented lines

--- src/test/java/com/project/transaction/authorizer/TransactionAuthorizerApplicationTests.java
+++ src/test/java/com/project/transaction/authorizer/TransactionAuthorizerApplicationTests.java
@@ -11,3 +11,10 @@ class TransactionAuthorizerApplicationTests {
 	}
 
 }
+
+//{"account": {"id": 1, "active-card": true, "available-limit": 100}}
+//{"transaction": {"id": 1, "merchant": "Burger King", "amount": 90,"time":"2019-02-13T10:00:00.000Z"}}
+
+// {"account": {"id": 1, "active-card": true, "available-limit": 100}}
+// {"account": {"id": 2, "active-card": true, "available-limit": 350}}
+// {"account": {"id": 4, "active-card": false, "available-limit": 350}}
